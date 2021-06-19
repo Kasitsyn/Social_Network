8bed2db23eca9e938323e8ae924619b678726f02
Kasitsyn <kasitsyn@gmail.com>
2021-06-19 18:33:43 +0700

    Added props for POST component

--- react1/src/Components/Profile/MyPosts/MyPosts.jsx
+++ react1/src/Components/Profile/MyPosts/MyPosts.jsx
@@ -7,10 +7,9 @@ const MyPosts = () => {
             <div>
                 New Post
                 <div className={s.posts}>
-                    <Post/>
-                    <Post/>
-                    <Post/>
-                    <Post/>
+                    <Post message="Hi, how are you?"/>
+                    <Post message="It's my first post"/>
+                    
                 </div>
             </div>
         </div>
