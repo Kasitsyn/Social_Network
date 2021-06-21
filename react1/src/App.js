c9ff512d457f13c47444ed8f4bc7094ac2bc8a0d
Kasitsyn <kasitsyn@gmail.com>
2021-06-21 19:36:03 +0700

    Added BrouserRouter module, Created Route paths at Navbar component

--- react1/src/App.js
+++ react1/src/App.js
@@ -4,18 +4,23 @@ import Header from './Components/Header/Header'
 import Navbar from './Components/Navbar/Navbar'
 import Profile from './Components/Profile/Profile'
 import Dialogs from './Components/Dialogs/Dialogs';
-
+import { BrowserRouter, Route } from 'react-router-dom';
 const App = () => {
 
   return (
     <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Profile />
-      </div>
-      
-      {/* <Dialogs /> */}
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path='/profile' component={Profile} />
+          <Route path='/dialogs' component={Dialogs} />
+          <Route path='/news' component={Profile} />
+          <Route path='/music' component={Profile} />
+          <Route path='/settings' component={Profile} />
+        </div>
+      </BrowserRouter>
+
 
     </div >
   );
