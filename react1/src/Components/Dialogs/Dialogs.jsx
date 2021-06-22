3ed48df6a0c98b2cdfaf42c6a19ccc0a2fa251ba
Kasitsyn <kasitsyn@gmail.com>
2021-06-22 15:38:50 +0700

    Modified <Dialog> component

--- react1/src/Components/Dialogs/Dialogs.jsx
+++ react1/src/Components/Dialogs/Dialogs.jsx
@@ -1,7 +1,27 @@
-const Dialogs = () => {
+import s from './Dialogs.module.css'
+
+const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItem}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Алексей
+                </div>
+                <div className={s.dialog}>
+                    Ольга
+                </div>
+                <div className={s.dialog}>
+                    Олег
+                </div>
+                <div className={s.dialog}>
+                    Константин
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hey!</div>
+                <div className={s.dialog}>How are you?</div>
+                <div className={s.dialog}>Thanks for IT!</div>
+            </div>
         </div>
     )
 
